3e261a8661c19206f206d4e200680cac6450e5d2
Alex Maina <lexizgichbuoy@gmail.com>
2023-11-29 19:08:03 +0000

    catch

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+function handleResponseFromAPI(promise) {
+  return (promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return ({ status: 200, body: 'success' });
+    })
+    .catch((error) => { return(new Error());
+    })
+  );
+}
+
+export default handleResponseFromAPI;
