e4a00093453d1f27d13a1ee613c761abda21e119
Alex Maina <lexizgichbuoy@gmail.com>
2023-11-29 16:04:43 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,1 +1,7 @@
-function getResponseFromAPI
+function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve('This is the response from the API!');
+  });
+}
+
+export default getResponseFromAPI;
