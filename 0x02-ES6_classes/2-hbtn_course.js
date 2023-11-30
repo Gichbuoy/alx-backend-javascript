80c984b97dd1850bed5b896dcd32ab771fb9ee6b
Alex Maina <lexizgichbuoy@gmail.com>
2023-11-30 21:21:16 +0000

    getters && setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,60 +1,38 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Name must be a number');
-    }
-    if (Array.isArray(students)) {
-      this.arrayIsOfStrings(students);
-      this._students = students;
-    }
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    if (!Number.isInteger(length)) throw new TypeError('length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
 
-    this._length = length;
     this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
+  set name(val) {
+    if (typeof val !== 'string') throw new TypeError('name must be a string');
+    this._name = val;
+  }
+
   get length() {
     return this._length;
+  }
+
+  set length(val) {
+    if (!Number.isInteger(val)) throw new TypeError('length must be a number');
+    this._length = val;
   }
 
   get students() {
     return this._students;
   }
 
-  set name(value) {
-    if (typeof value === 'string') {
-      this._name = value;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
-  }
-
-  set length(value) {
-    if (typeof length === 'number') {
-      this._length = value;
-    } else {
-      throw new TypeError('Length must be a number');
-    }
-  }
-
-  set students(value) {
-    if (Array.isArray(value)) {
-      this.arrayIsOfStrings(value);
-      this._students = value;
-    }
-  }
-
-  arrayIsOfStrings(array) {
-    array.forEach(element => {
-      if (typeof element !== 'string') {
-        throw new TypeError('Students must be an array of strings');
-      }
-    });
+  set students(val) {
+    if (!Array.isArray(val)) throw new TypeError('students type must be an Array');
+    this._students = val;
   }
 }
--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,3 +1,4 @@
+import Currency from "3-currency";
 
 export default class Pricing {
   constructor(amount, currency) {
